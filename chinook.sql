03f7131fec643eeec39131f76170b6dbb5d408cc
Cassie Boyd <cassandra.bacon@gmail.com>
2020-02-16 14:51:44 -0600

    Query 5

--- chinook.sql
+++ chinook.sql
@@ -24,3 +24,8 @@ WHERE Country = "Brazil";
 SELECT FirstName, LastName, Title
 FROM Employee
 WHERE Title = "Sales Support Agent";
+
+-- unique_invoice_countries.sql: Provide a query showing a unique/distinct list of billing countries from the Invoice table.
+
+SELECT DISTINCT BillingCountry
+FROM Invoice;
