90bed9798fa2a6286e0984b4e61cde74bbfdd29b
Cassie Boyd <cassandra.bacon@gmail.com>
2020-02-16 15:32:31 -0600

    Query 7

--- chinook.sql
+++ chinook.sql
@@ -36,3 +36,10 @@ SELECT e.FirstName, e.LastName, i.InvoiceDate, i.Total
 FROM Employee e, Invoice i, Customer c
 WHERE e.EmployeeId = c.SupportRepId
 AND c.CustomerId = i.CustomerId;
+
+-- invoice_totals.sql: Provide a query that shows the Invoice Total, Customer name, Country and Sale Agent name for all invoices and customers.
+
+SELECT e.FirstName ||" "|| e.LastName AS EmployeeName, i.Total, i.BillingCountry, c.FirstName ||" "|| c.LastName AS CustomerName
+FROM Employee e, Invoice i, Customer c
+WHERE e.EmployeeId = c.SupportRepId
+AND c.CustomerId = i.CustomerId;
